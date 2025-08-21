4cbff084f11a85fb9ea5a06f0f07cb214d5d3cd4
sathvik-04 <sathvikshetty0404@gmail.com>
2025-08-21 23:05:13 +0530

    added components in app.jsx and created a new file search.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 
 
-import React from 'react'
+
+import { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
+
+  const[searchTerm,setSearchTerm]= useState('');
+
   return (
     <main>
   <div  className='pattern'/>
-  <div className='"wrapper"'>
+  <div className="wrapper">
     <header>
       <img src="./hero.png" alt="hero banner"/>
       <h1>Find <span className='text-gradient'>movies</span> with Ease</h1>
     </header>
 
-<p>Search</p>
+<Search searchTerm={searchTerm} setSearchTerm= {setSearchTerm}/>
+
+<h1 className='text-pink'>{searchTerm}</h1>
 
 
 
