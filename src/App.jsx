31913fdbb5f8b42c291440c14957f207daaa241a
sathvik-04 <sathvikshetty0404@gmail.com>
2025-08-21 21:44:59 +0530

    Initial commit: setup Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+  <div  className='pattern'/>
+  <div className='"wrapper"'>
+    <header>
+      <img src="./hero.png" alt="hero banner"/>
+      <h1>Find <span className='text-gradient'>movies</span> with Ease</h1>
+    </header>
+
+<p>Search</p>
+
+
+
+
+  </div>
+    </main>
+  )
+}
+
+export default App
